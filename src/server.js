801f5140b596ed5b6195048499fd43aab1a9aca2
David Armendariz <darmendariz1998@outlook.com>
2021-04-10 14:22:52 -0500

    Separated auth server from posts server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
 import { config } from 'dotenv';
+import authenticateToken from './authenticatToken';
 
 config();
 
@@ -15,24 +15,5 @@ const posts = [
 app.get('/posts', authenticateToken, (req, res) => {
   res.json(posts.filter((post) => post.username === req.user.name));
 });
-
-app.post('/login', (req, res) => {
-  // Authenticate user
-  const username = req.body.username;
-  const user = { name: username };
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  res.json({ accessToken });
-});
-
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (!token) return res.sendStatus(401);
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-}
 
 app.listen(3000);
