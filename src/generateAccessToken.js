8f99b8847e7663e0a3d7d6bf8a116a258c7078f9
David Armendariz <darmendariz1998@outlook.com>
2021-04-10 15:12:50 -0500

    Added jwt import

--- src/generateAccessToken.js
+++ src/generateAccessToken.js
@@ -1,3 +1,5 @@
+import jwt from 'jsonwebtoken';
+
 export default function generateAccessToken(user, options) {
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, options);
 }
--- src/generateRefreshToken.js
+++ src/generateRefreshToken.js
@@ -1,3 +1,5 @@
+import jwt from 'jsonwebtoken';
+
 export default function generateRefreshToken(user) {
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
 }
